14de2c12a2a937060e8d10216a84455aa652a97a
umangag07 <25umangagarwal@gmail.com>
2022-06-02 16:32:16 +0530

    feat: add reverse proxy nginx server

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,17 +1,19 @@
 const userController = require("../controllers/userController");
 const express = require("express");
 const Router = express.Router();
+const { auth } = require("../middleware/authMiddleware");
+
 
 Router.route("/")
-  .get(userController.findAllUsers)
+  .get(auth,userController.findAllUsers)
 Router.route("/signup")
   .post(userController.createUser);
 Router.route("/login")
   .post(userController.login);
-Router.route("/email").post(userController.findUserByEmail);
+Router.route("/email").post(auth,userController.findUserByEmail);
 Router.route("/:id")
-  .get(userController.findSingleUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+  .get(auth,userController.findSingleUser)
+  .patch(auth,userController.updateUser)
+  .delete(auth,userController.deleteUser);
 
 module.exports = Router;
